7a2355d889843fe92c1e7389a4928348315fbaad
Romina Piria <romipiria@gmail.com>
2023-11-08 19:09:04 -0300

    added loader for the files in scope of commit, refactored error and goBack button code in each container

--- src/containers/SendGift/index.jsx
+++ src/containers/SendGift/index.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useContext } from 'react';
 import GoBack from 'Components/GoBack';
 import Error from 'Components/Error';
+import { ColorRing } from 'react-loader-spinner';
 import getUsers from '../../api/users';
 import UserGiftList from '../../components/UserGiftList';
 import UserContext from '../../context/UserContext';
@@ -12,8 +13,10 @@ const SendGift = () => {
   const [userList, setUserList] = useState([]);
   const { loggedUser, setLoggedUser } = useContext(UserContext);
   const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const getAllUsers = async () => {
+    setLoading(true);
     const users = await getUsers();
     const modifiedUsers = removeLoggedUserFromList(users.data);
 
@@ -22,6 +25,7 @@ const SendGift = () => {
     } else {
       setUserList(modifiedUsers);
     }
+    setLoading(false);
   };
 
   const removeLoggedUserFromList = (users = []) => {
@@ -39,22 +43,42 @@ const SendGift = () => {
     getAllUsers();
   }, []);
 
-  if (error) {
+  const renderSendGift = () => {
+    if (!loading) {
+      if (!error) {
+        return (
+          <div className="sendGift">
+            {userList.length > 0 && <GoBack />}
+            <UserGiftList users={userList} />
+          </div>
+        );
+      }
+      return (
+        <Error title="No data found" image={noData} text="Error" />
+      );
+    }
     return (
-      <Error
-        title="No data found"
-        image={noData}
-        text="No data found"
+      <ColorRing
+        visible
+        height="80"
+        width="80"
+        ariaLabel="blocks-loading"
+        wrapperStyle={{}}
+        wrapperClass="blocks-wrapper"
+        colors={[
+          '#fafafa',
+          '#e8e6e6',
+          '#d4d2cf',
+          '#b5b1aa',
+          '#918c83',
+          '#69635b',
+          '#403c35',
+        ]}
       />
     );
-  }
+  };
 
-  return (
-    <div className="sendGift">
-      {userList.length > 0 && <GoBack />}
-      <UserGiftList users={userList} />
-    </div>
-  );
+  return <div>{renderSendGift()}</div>;
 };
 
 export default SendGift;
