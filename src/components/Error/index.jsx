f59fadaa47b56a69128564fbed817d5bcd0bd81a
Romina Piria <romipiria@gmail.com>
2023-11-10 16:27:49 -0300

    fixed style issues on category buttons and products in cart view section. fixed error when showing list of users to send gift

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -2,19 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './styles.scss';
 
-const Error = ({ title, image, text, className }) => (
+const Error = ({ image, className }) => (
   <div className={className}>
-    <h2>{title}</h2>
-    <img src={image} alt={text} className="error__image"></img>
-    <p>{text}</p>
+    <img src={image} alt="Error" className="error__image"></img>
   </div>
 );
 
 Error.propTypes = {
   className: PropTypes.string,
   image: PropTypes.string.isRequired,
-  text: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
 };
 
 export default Error;
