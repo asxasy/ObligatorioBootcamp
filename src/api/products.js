23d7f5df0229ee407e8f67ab1b6e5bebfd5e0fef
Romina Piria <romipiria@gmail.com>
2023-11-10 18:52:17 -0300

    fixed minimum errors of style in cartView

--- src/api/products.js
+++ src/api/products.js
@@ -5,7 +5,7 @@ const getProducts = () => instance.get('/products');
 const getProductDetails = (id) => instance.get(`products/${id}`);
 
 //For trying error funtioning
-//const getProductDetails = () => instance.get(`products/200`);
+//const getProducts = () => instance.get('/product');
 
 const getCategories = () => instance.get(`products/categories`);
 
